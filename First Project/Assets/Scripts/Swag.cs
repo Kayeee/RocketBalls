571c45d40f2a08eb9d31a064645a7d7386a5b571
kevin <kevin.everly-1@ou.edu>
2016-12-09 20:23:34 -0700

    initial commit

---
+++ First Project/Assets/Scripts/Swag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swag : MonoBehaviour {
+
+    [SerializeField]
+    private float dopeSwagger = 5;
+    public float swagger = 1000000;
+
+	// Use this for initialization
+	void Start () {
+
+        //dopeSwagger = GetComponent<Transform>().position.x;
+
+        Vector3 newPosition = GetComponent<Transform>().position;
+        newPosition.x = dopeSwagger;
+
+        transform.position = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
